8ec00029b2e2dfbf90f44d561981a79b04ce4ada
catri-01 <catrimaouene@gmail.com>
2023-10-06 16:44:13 +0200

    Ajout d'une nouvelle fonction

--- example.py
+++ example.py
@@ -1,4 +1,23 @@
-def git_opeation():
- print("I am adding example.py file to the remote repository.")
-git_opeation()
+import psutil
+import os
 
+# def git_operation():
+#  print("I am adding example.py file to the remote repository.")
+
+
+def get_disk_usage():
+    
+    disk_usage = psutil.disk_io_counters()
+    return disk_usage.read_count, disk_usage.write_count
+
+def main():
+   read_count, write_count = get_disk_usage()
+
+   print(f"Opérations de lecture sur le disque: {read_count} ")
+   print(f"Opérations de lecture sur le disque: {write_count} ")
+
+disk_usage = get_disk_usage()
+print(disk_usage)
+
+# if __name__ == "__main__":
+#     main
